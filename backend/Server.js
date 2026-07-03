930c18cf8de8fae2817f93a34586d140386203ae
omsingh1240-oss <omsingh1240@gmail.com>
2026-07-03 15:41:28 +0530

    Version 1.5 - Connected backend to MongoDB Atlas

--- backend/Server.js
+++ backend/Server.js
@@ -1,10 +1,15 @@
 import dotenv from "dotenv";
 import app from "./config/app.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+// Connect Database First
+await connectDB();
+
+// Start Server
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
--- backend/data/users.js
+++ backend/data/users.js
@@ -3,7 +3,8 @@ const users = [
     id: 1,
     name: "Admin",
     email: "admin@crm.com",
-    password: "123456",
+    password:
+      "$2b$10$0m2M3D1eJq1OZ0q4nYhJc.uTqgM3M8S1K4r9Aq5N8QdKzWm5v9B5K",
     role: "admin",
   },
 ];
